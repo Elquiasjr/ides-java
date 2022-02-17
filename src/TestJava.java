4e6c8f9c69853578aa9f4599a4a308ad746c613c
Elquiasjr <elquiasj@gmail.com>
2022-02-17 12:21:18 -0300

    first commit

---
+++ src/TestJava.java
@@ -0,0 +1,21 @@
+import pacote.dio.Cat;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		Cat gato = new Cat(null, null, null);
+		Livros livros = new Livros();
+		System.out.println(gato);
+		/*String casa = "azul";
+		System.out.println("Hello World! " + casa);
+		
+		*/
+	
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
